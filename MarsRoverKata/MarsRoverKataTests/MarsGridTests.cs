e1dbd349b159b0fc7e546f62956d7e45f0432532
pololii <33549413+pololii@users.noreply.github.com>
2018-08-08 08:33:37 +0200

    WIP: added first MarsGrid test

---
+++ MarsRoverKata/MarsRoverKataTests/MarsGridTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarsRoverKataApi;
+using System.Collections.Generic;
+
+namespace MarsRoverKataTests
+{
+    [TestClass]
+    public class MarsGridTests
+    {
+        [TestMethod]
+        public void Rover_Turns_Left_From_North()
+        {
+
+            var gridDimension = new Point(1, 1);
+            var obstacleList = new List<Obstacle>();
+
+            var marsGrid = new MarsGrid(gridDimension, obstacleList);
+
+            var newDirection = marsGrid.TurnRoverLeft(Direction.North);
+
+            Assert.IsTrue(newDirection == Direction.West);
+
+        }
+
+
+    }
+}
