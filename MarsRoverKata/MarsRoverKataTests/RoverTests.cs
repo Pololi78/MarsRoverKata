85810e88267520259609286c112f3fe51a6956aa
pololii <33549413+pololii@users.noreply.github.com>
2018-08-09 22:15:23 +0200

    WIP: reorganized tests

--- MarsRoverKata/MarsRoverKataTests/RoverTests.cs
+++ MarsRoverKata/MarsRoverKataTests/RoverTests.cs
@@ -76,60 +76,6 @@ namespace MarsRoverKataTests
 
         }
 
-        [TestMethod]
-        public void Rover_Reaches_Correct_Position_1()
-        {
-
-            var gridDimension = new Point(2,2);
-            var obstacleList = new List<Obstacle>();
-            
-            var marsGrid = new MarsGrid(gridDimension, obstacleList);
-
-            var rover = new Rover(marsGrid, new Point(1, 1), Direction.North);
-
-            var roverInfo = rover.MoveAndTurn("FF");
-
-            Assert.IsTrue(roverInfo.RoverDirection == Direction.North &&
-                          roverInfo.RoverPosition.X == 1 &&
-                          roverInfo.RoverPosition.Y == 1);
-        }
-
-        [TestMethod]
-        public void Rover_Reaches_Correct_Position_2()
-        {
-
-            var gridDimension = new Point(5, 5);
-            var obstacleList = new List<Obstacle>();
-
-            var marsGrid = new MarsGrid(gridDimension, obstacleList);
-
-            var rover = new Rover(marsGrid, new Point(1, 2), Direction.North);
-
-            var roverInfo = rover.MoveAndTurn("LFLFLFLFF");
-
-            Assert.IsTrue(roverInfo.RoverDirection == Direction.North &&
-                          roverInfo.RoverPosition.X == 1 &&
-                          roverInfo.RoverPosition.Y == 3);
-        }
-
-        [TestMethod]
-        public void Rover_Reaches_Correct_Position_3()
-        {
-
-            var gridDimension = new Point(5, 5);
-            var obstacleList = new List<Obstacle>();
-
-            var marsGrid = new MarsGrid(gridDimension, obstacleList);
-
-            var rover = new Rover(marsGrid, new Point(3, 3), Direction.East);
-
-            var roverInfo = rover.MoveAndTurn("FFRFFRFRRF");
-
-            Assert.IsTrue(roverInfo.RoverDirection == Direction.East &&
-                          roverInfo.RoverPosition.X == 5 &&
-                          roverInfo.RoverPosition.Y == 1);
-        }
-
         #endregion
     }
 }
